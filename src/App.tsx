48f889a850d67d6942072ec5a3640a874bc5fdb0
Amar Prakash Pandey <amar.om1994@gmail.com>
2024-04-09 19:28:35 +0000

    feat: show cordinates using leaflet container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css';
-import './App.css';
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 function App() {
   return (
-    <div className="App">
-      <p>GeohashViz.com</p>
-    </div>
+    <MapContainer center={[26.461624, 80.329236]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
   );
 }
 
